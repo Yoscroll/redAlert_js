d58140a4650e075c58c2c6a2fbb5350b7e291b8a
Yoscroll <ianhenry.c808@gmail.com>
2016-12-09 09:02:33 -1000

    added loop to change all elements

--- js/app.js
+++ js/app.js
@@ -3,6 +3,13 @@ console.log("Sanity Check");
 function hello(){
   console.log("hello");
   var changeDiv= document.getElementById('divElement');
-  document.innerHTML = "hi";
+  changeDiv.innerHTML = "wat";
   console.log(changeDiv);
+}
+
+function soFast(){
+  var classElements = document.getElementsByClassName('same');
+  for (var i = 0; i < classElements.length; i++) {
+    classElements[i].innerHTML = "wat";
+  }
 }
